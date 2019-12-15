53a51590067e531290d0ebe6767a3fddfd79a1a2
frantchessico <jaimeinoque20@gmail.com>
2019-12-15 12:55:26 +0200

    primerio commit de oram

---
+++ src/app/shared/employee.model.ts
@@ -0,0 +1,10 @@
+export class Employee {
+    id: string;
+    fullName: string;
+    empCode: string;
+    position: string;
+    mobile: string;
+    gender?: string;
+    provincia?: string;
+    createdAt?: any;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: '#',
+    authDomain: '#',
+    databaseURL: '#',
+    projectId: '#',
+    storageBucket: '#',
+    messagingSenderId: '#',
+    appId: '#',
+    measurementId: '#'
+  }
+};
+
